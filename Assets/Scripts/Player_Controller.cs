06d5a33b6ef733ad14dc639ba57c6682c1c54527
DESKTOP-NT34NVN\LENOVO <dmsohel07@gmail.com>
2020-12-24 15:29:14 +0530

    Added charecter Movement and jumping

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,28 +5,77 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
+    public float yjump, xjump;
+    Rigidbody2D _rigidbody2d;
+   
     // Start is called before the first frame update
-   /* void Start()
-    {
+    void Start()
+     {
         
-    }*/
+        _rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
+     }
 
     // Update is called once per frame
-  private void Update()
+    private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-     
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-       
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        RunAnimation(horizontal);
+        JumpAnimation(vertical);
+        PhysicalMovementJump(vertical);
+        PhysicalMovementRun(horizontal);
+        
+
+    }
+    void RunAnimation(float horizontal)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+    }
+    void JumpAnimation(float vertical)
+    {
+        if (vertical > 0)
+        {
+            animator.SetBool("HJump", true);
+        }
+        else
+            animator.SetBool("HJump", false);
+    }
+    void PhysicalMovementRun(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime; 
+        transform.position = position;
+    }
+    void PhysicalMovementJump(float vertical)
+    {
+        if (vertical > 0)
+        {
+            Vector3 scale = transform.localScale;
+            if (scale.x >= 0)
+            {
+
+                _rigidbody2d.AddForce(new Vector2(xjump, yjump), ForceMode2D.Impulse);
+            }
+              else
+
+               _rigidbody2d.AddForce(new Vector2(-xjump, yjump), ForceMode2D.Impulse);
+        }
     }
 }
+
+   
+       
+
