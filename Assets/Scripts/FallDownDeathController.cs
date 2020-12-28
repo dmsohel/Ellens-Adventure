5095ba1d174f1b175ad9bfbcbe3b3fed9da28d8b
DESKTOP-NT34NVN\LENOVO <dmsohel07@gmail.com>
2020-12-28 11:31:38 +0530

    Added Collectibles and Responsive UI

---
+++ Assets/Scripts/FallDownDeathController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallDownDeathController : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player_Controller>() != null)
+        {
+            Player_Controller player_Controller = collision.gameObject.GetComponent<Player_Controller>();
+            player_Controller.DeathAnimation();
+           
+        }
+        
+        SceneManager.LoadScene(0);
+
+    }
+}
---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player_Controller>() != null)
+        {
+            Player_Controller player_Controller = collision.gameObject.GetComponent<Player_Controller>();
+            player_Controller.PickUpKey();
+          
+            Destroy(gameObject);
+        }
+    }
+}
